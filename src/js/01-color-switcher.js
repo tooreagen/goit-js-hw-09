2e105cd6be65157a4face01b31d2bd91ff7f059b
tooreagen <tooreagen@gmail.com>
2023-01-08 11:37:48 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBackgroundColor() {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor(); 
+    }, 1000);
+}
+
+function stopInterval() {
+    clearInterval(intervalId);
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+}
+
+buttonStart.addEventListener("click", setBackgroundColor);
+buttonStop.addEventListener("click", stopInterval);
